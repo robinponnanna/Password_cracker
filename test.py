55e0773b03f6ca2dc589388533dbbc1263105146
robinponnanna <ponnannarobin@gmail.com>
2025-11-09 00:11:29 +0530

    tested threading

--- test.py
+++ test.py
@@ -6,25 +6,18 @@ def func1(sec):
     time.sleep(sec)
     print(f"done {sec}")
 
-# def func2():
-#     print("sleeping for 3 sec")
-#     time.sleep(3)
-#     print("done")
-
-# def func3():
-#     print("sleeping for 1 sec")
-#     time.sleep(1)
-#     print("done")
-
-
 t1 = threading.Thread(target=func1,args=[5])
 t2 = threading.Thread(target=func1,args=[3])
 t3 = threading.Thread(target=func1,args=[1])
 t1.start()
 t2.start()
 t3.start()
+print(t1)
+print(t2)
+print(t3)
 t1.join()
 t2.join()
 t3.join()
 
 print("completed")
+
